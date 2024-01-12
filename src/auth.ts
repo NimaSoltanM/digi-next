945b23faa0f712149bcfd72b66767ab401cf472e
NimaSoltanM <manimanavi801@gmail.com>
2024-01-12 19:26:05 +0330

    user id

--- src/auth.ts
+++ src/auth.ts
@@ -13,6 +13,28 @@ export const {
   pages: {
     signIn: '/sign-in',
   },
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await db.user.findUnique({
+        where: { id: token.sub },
+      });
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
 
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
